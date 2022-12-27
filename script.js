baf73c94bc1c487b9aacc394901977a23f05a587
Gustavo Pucienik <gomespucienik@gmail.com>
2022-12-26 22:08:39 -0300

    salvando preferencia de tema de usuario

--- script.js
+++ script.js
@@ -2,7 +2,27 @@
 
 const mudaTemaBtn = document.querySelector("#muda-tema")
 
-mudaTemaBtn.addEventListener("change", function(){
-
+function mudatema(){
     document.body.classList.toggle("dark")
+}
+
+loadtheme()
+
+function loadtheme(){
+    const dark = localStorage.getItem("dark")
+
+    if(dark)[
+        mudatema()
+    ]
+}
+
+
+mudaTemaBtn.addEventListener("change", function(){
+    mudatema()
+
+    localStorage.removeItem("dark")
+
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("dark", 1)
+    }
 })
